c86fde14cdeeb9bbaa4f9215eb1db784eaf24c10
pepe <jose.vacaslocas@gmail.com>
2020-02-19 20:35:39 -0700

    se buscan numeros pares del 1 al 10

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Even_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine("es par ");
+                }
+            }
+        }
+    }
+}
